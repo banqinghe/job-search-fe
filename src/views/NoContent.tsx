b39b2aa5423ff2f03f496deaf49b288829ac139f
banqinghe <qingheban@qq.com>
2021-11-26 17:59:24 +0800

    feat: add 404 page

---
+++ src/views/NoContent.tsx
@@ -0,0 +1,10 @@
+function NoContent() {
+  return (
+    <div className="flex-1 mx-auto mt-64 text-center">
+      <h1 className="mb-4 text-2xl font-bold">404 NOT FOUND</h1>
+      <p>你访问了错误的地址</p>
+    </div>
+  );
+}
+
+export default NoContent;
