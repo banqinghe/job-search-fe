40829ff4e10c83ad1089e7fbc6857890c22537a5
banqinghe <qingheban@qq.com>
2022-01-03 13:27:41 +0800

    feat: update static file url

--- server/consts.py
+++ server/consts.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:2000126ban@127.0.0.1/job"
-STORAGE_BASE_URL = "http://0.0.0.0:8001"
+STORAGE_BASE_URL = "//124.223.21.196:8001"
 STORAGE_DIR = Path(__file__).parent / "storage"
